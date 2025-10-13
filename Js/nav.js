abd1beb95a33cd9afead0ccddb492ee227130e35
MoSu1248 <mohammedsuhailr2@gmail.com>
2025-10-13 13:14:17 +0200

    third commit

--- Js/nav.js
+++ Js/nav.js
@@ -1,25 +1,37 @@
+const sections = document.querySelectorAll(".section");
+const links = document.querySelectorAll(".link-styling");
+const wrapper = document.querySelector(".content-wrapper");
 
-const sections = document.querySelectorAll('.section');
-const links = document.querySelectorAll('.link-styling');
-const wrapper = document.querySelector('.content-wrapper');
+const mobileOpenBtn = document.querySelector(".mobile-icon");
+const mobileCloseBtn = document.querySelector(".mobile-close");
+const mobileMenu = document.querySelector(".mobile-menu");
 
-links.forEach(link => {
-  link.addEventListener('click', (e) => {
+links.forEach((link) => {
+  link.addEventListener("click", (e) => {
     e.preventDefault();
 
-    const targetId = link.getAttribute('href').slice(1);
+    const targetId = link.getAttribute("href").slice(1);
 
     wrapper.className = `content-wrapper ${targetId}-bg`;
 
-    links.forEach(l => l.classList.remove('active-link'));
-    link.classList.add('active-link');
+    links.forEach((l) => l.classList.remove("active-link"));
+    link.classList.add("active-link");
+    mobileMenu.classList.remove("visible");
 
-    sections.forEach(section => {
+    sections.forEach((section) => {
       if (section.id === targetId) {
-        section.classList.remove('hidden');
+        section.classList.remove("hidden");
       } else {
-        section.classList.add('hidden');
+        section.classList.add("hidden");
       }
     });
   });
+});
+
+mobileOpenBtn.addEventListener("click", (e) => {
+  mobileMenu.classList.add("visible");
+});
+
+mobileCloseBtn.addEventListener("click", (e) => {
+  mobileMenu.classList.remove("visible");
 });
